31197b11fac046ab55d09ed512a1cfeb895a1400
Ash Feijoo <AshleyFeijoo1@gmail.com>
2019-05-29 14:34:01 -0500

    initial

--- server.js
+++ server.js
@@ -10,12 +10,15 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
-app.use(express.static("./app/public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Sets up the Express app to handle data parsing
+app.use(express.static("./app/public"));
+
+
 //ROUTER
-require('./app/routing/apiRoutes')(app); 
+// require('./app/routing/apiRoutes')(app); 
 require('./app/routing/htmlRoutes')(app);
 
 // Starts the server to begin listening
